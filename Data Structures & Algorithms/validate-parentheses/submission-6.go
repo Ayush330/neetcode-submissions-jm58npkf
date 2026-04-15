89f14941f33c0053493cfe986938a5787e8900b0
Ayush Kumar Anand <anand199814@gmail.com>
2026-04-15 16:36:17 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.go
@@ -0,0 +1,21 @@
+func isValid(s string) bool {
+    stack := []byte{}
+	for i:=0; i<len(s); i++{
+		if s[i] == '(' || s[i] == '{' || s[i] == '['{
+			stack = append(stack, s[i])
+		}else{
+			lenStack := len(stack)
+			if lenStack > 0{
+				topEl := stack[lenStack-1]
+				if s[i] == ')' && topEl == '(' || s[i] == '}' && topEl == '{' || s[i] == ']' && topEl == '['{
+					stack = stack[:lenStack-1]
+				}else{
+					return false
+				}
+			}else{
+				stack = append(stack, s[i])
+			}
+		}
+	}
+	return len(stack) == 0
+}
