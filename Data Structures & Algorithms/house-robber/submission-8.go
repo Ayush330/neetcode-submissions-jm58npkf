192317e0c8cda9de2ecb16ce6bcf3e783fbaac3d
Ayush Kumar Anand <anand199814@gmail.com>
2026-05-13 13:22:52 +0530

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.go
@@ -0,0 +1,35 @@
+func rob(nums []int) int {
+    // (dp[i] = dp[i-1], dp[i-2]+nums[i])
+	l := len(nums)
+
+	if l == 1{
+		return nums[l-1]
+	}
+
+	if l == 2{
+		return max(nums[l-1], nums[l-2])
+	}
+
+	//dp := make([]int, l)
+
+	//dp[0] = nums[0]
+	first := nums[0]
+	//dp[1] = max(nums[0], nums[1])
+	second := max(nums[0], nums[1])
+
+	
+	for i:=2; i<l; i++{
+		//dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+		first, second = second, max(second, first + nums[i])
+	}
+
+	return second
+}
+
+
+func max(a, b int)int{
+	if a > b{
+		return a
+	}
+	return b
+}
