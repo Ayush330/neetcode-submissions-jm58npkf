df0af6a352dabd6cb72ae4ebddebd33955f17920
Ayush Kumar Anand <anand199814@gmail.com>
2026-08-08 17:31:54 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] lookup = new int[26];
+        for(int i=0; i<s.length(); i++){
+            int posn = s.charAt(i) - 'a';
+            lookup[posn]++;
+        }
+        for(int i=0; i<t.length(); i++){
+            int posn = t.charAt(i) - 'a';
+            lookup[posn]--;
+        }
+        // verify now
+        for(int i=0; i<lookup.length; i++){
+            if(lookup[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
